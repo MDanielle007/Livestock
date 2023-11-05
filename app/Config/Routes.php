304d39fe8c595b680ec15a39bccc262eaf423041
MDanielle007 <marc.danielle.cabatay219@gmail.com>
2023-11-06 06:40:45 +0800

    Add Farmer Registration

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -14,3 +14,6 @@ $routes->post('/farmer/addLivestock', 'FarmerController::addLivestock');
 $routes->get('/farmer/getAllFarmerLivestock/(:any)', 'FarmerController::getAllFarmerLivestock/$1'); 
 // gawa ka ng routes para sa function na ginawa mo
 $routes->get('/farmer/getOneLivestock/(:segment)/(:segment)', 'FarmerController::getOneLivestock/$1/$2'); 
+
+$routes->post('/admin/registerFarmer', 'UserController::registerFarmer');
+$routes->get('/getUserLastID', 'UserController::getUserLastID');
--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -58,6 +58,89 @@ class UserController extends ResourceController
         return $this->respond($result,200);
     }
 
+    public function registerFarmer(){
+        helper(['form']);
+        $rules = [
+            'Username' => ['rules' => 'required|min_length[4]|max_length[255]'],
+            'Password' => ['rules' => 'required|min_length[8]|max_length[255]'],
+            'Email' => ['rules' => 'required|min_length[6]|max_length[255]|valid_email'],
+            'Firstname' => ['rules' => 'required|max_length[255]'],
+            'Middlename' => ['rules' => 'max_length[255]'],
+            'Lastname' => ['rules' => 'required|max_length[255]'],
+            'Date_Of_Birth' => ['rules' => 'valid_date'], // You may need a custom rule for date validation.
+            'Gender' => ['rules' => 'required|in_list[Male,Female,Other]'],
+            'Civil_Status' => ['rules' => 'required|in_list[Single,Married,Widowed,Divorced]'],
+            'Sitio' => ['rules' => 'max_length[255]'],
+            'Barangay' => ['rules' => 'required|max_length[255]'],
+            'City' => ['rules' => 'required|max_length[255]'],
+            'Province' => ['rules' => 'required|max_length[255]'],
+            'Phone_Number' => ['rules' => 'max_length[11]'], // Adjust max length as needed.
+        ];
+
+        // $validation = \Config\Services::validation();
+
+        // $validation->setRules($rules);
+
+        // return $this->respond($validation,200);
+        
+        if($this->validate($rules)){
+            // Get the input data using the request object
+            $data = [
+                'Username' => $this->request->getVar('Username'),
+                'Password' => password_hash($this->request->getVar('Password'), PASSWORD_DEFAULT),
+                'Email' => $this->request->getVar('Email'),
+                'Firstname' => $this->request->getVar('Firstname'),
+                'Middlename' => $this->request->getVar('Middlename'),
+                'Lastname' => $this->request->getVar('Lastname'),
+                'Date_Of_Birth' => $this->request->getVar('Date_Of_Birth'),
+                'Gender' => $this->request->getVar('Gender'), // 'Male' or 'Female' or 'Other'
+                'Civil_Status' => $this->request->getVar('Civil_Status'), // 'Single','Married','Widowed', or 'Divorced'
+                'Sitio' => $this->request->getVar('Sitio'),
+                'Barangay' => $this->request->getVar('Barangay'),
+                'City' => $this->request->getVar('City'),
+                'Province' => $this->request->getVar('Province'),
+                'Phone_Number' => $this->request->getVar('Phone_Number'),
+                'User_Role' => $this->request->getVar('User_Role'),
+            ];
+
+            $this->userAccounts->save($data);
+
+            $userID = $this->getUserLastID();
+            $yearsFarming = $this->request->getVar('YearsFarming');
+
+            $this->addFarmerProfile($userID,$yearsFarming);
+            
+            return $this->respond(['message' => 'Registered Successfully'],200);
+        }else{
+            $response = [
+                'errors' => $this->validator->getErrors(),
+                'message' => 'Invalid Inputs'
+            ];
+            return $this->respond($response );
+        }
+    }
+
+    public function getUserLastID() {
+        // Use CodeIgniter Query Builder to get the maximum Livestock_ID.
+        $lastUserID = $this->userAccounts->selectMax('User_ID')->get()->getRow();
+
+        // Check if a result is found.
+        if ($lastUserID) {
+            $lastID = $lastUserID->User_ID;
+            return $lastID;
+        } else {
+            return 0;
+        }
+    }
+
+    private function addFarmerProfile($userID, $yearsFarming = 0){
+        $data = [
+            "User_ID" => $userID,
+            "Years_Of_Farming" => $yearsFarming,
+        ];
+        $this->farmerProfiles->save($data);
+    }
+
     public function loginAuth(){
         $session = session();
         $json = $this->request->getJSON();
