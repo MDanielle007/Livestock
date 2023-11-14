baa98b532297319e699bf3c19c194ee05e34f838
talawww <esaymatira@gmail.com>
2023-11-14 12:34:47 +0800

    add Sales API

--- app/Controllers/SalesController.php
+++ app/Controllers/SalesController.php
@@ -21,6 +21,62 @@ class SalesController extends BaseController
     }
 
     public function commitSalesTransaction(){
+        try{
+            $json = $this->request->getJSON();
+            $farmerID = $json->Farmer_ID;
+            $salesTotal = $json->Sales_Total_Price;
+            $amountPaid = $json->PaymentAmount;
+            $changeAmount = $json->ChangeAmount;
+
+            date_default_timezone_get('Asia/Manila')
+            $salesDate = date('Y-m-d');
+
+            $buyerName = $json->BuyerName;
+            $buyerType = $json->BuyerType;
+
+            $data = [
+                'Farmer_ID' => $farmerID,
+                'Sales_TotalPrice' => $salesTotal,
+                'PaymentAmount' => $amountPaid,
+                'ChangeAmount' => $changeAmount,
+                'Sales_Date' => $salesDate,
+                'Buyer_Name' => $buyerName,
+                'Buyer_Type' => $buyerType,
+            ];
+
+            $salesID =$this->getSalesLastID();
+            $response = $this->commitSoldLivestock($salesID, $json->soldLivestocks);
+
+            $this->salesTransaction->save($data);
+
+            $this->response(['message' => 'sales committed successfully'], 200);           
+        } catch (\Throwable $e) {
+            return $this->respond(["message" => "Error: " . $e->getMessage()]);
+        }
         
+    }
+
+    public function getSalesLastID() {
+        $lastSalesID = $this->salesTransaction->selectMax('Sales_ID')->get()->getRow();
+
+        if ($lastSalesID) {
+            $lastID = $lastSalesID->Sales_ID;
+            return $lastID;         
+        } else {
+            return 0;
+        }
+    }
+
+    private function commitSoldLivestocks($salesID){
+        $soldLivestocks = $this->request->getVar('soldLivestocks');
+        foreach ($soldLivestocks as $livestock) {
+            $data = [
+                'Sales_ID' => $salesID,
+                'Livestock_ID' => $livestock['Livestock_ID'],
+                'Livestock_Price' => $livestock['Livestock_Price'],
+            ];
+
+            $this->soldLivestocks->save($data);
+        }
     }
 }
