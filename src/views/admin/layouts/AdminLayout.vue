e05be92b8c8b08920d630f053337948eca740c9b
lyyyyy23 <130579065+lyyyyy23@users.noreply.github.com>
2023-11-15 14:04:10 +0800

    add login vue and others

--- src/views/admin/layouts/AdminLayout.vue
+++ src/views/admin/layouts/AdminLayout.vue
@@ -1,11 +1,22 @@
 <template>
     <div>
-        
+        <AdminAppBar/>
+        <AdminSidebar/>
+        <main>
+            <router-view></router-view>
+        </main>
     </div>
 </template>
 <script>
+import AdminAppBar from '@/components/admin/AdminAppBar.vue'
+import AdminSidebar from '@/components/admin/AdminSidebar.vue'
+
 export default {
-    
+    name:'admin-layout',
+    components:{
+        AdminAppBar,
+        AdminSidebar
+    }
 }
 </script>
 <style>
