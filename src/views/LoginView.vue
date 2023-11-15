6a1d4d1d3fc0d00cdd398f68c2fc4664d2948f99
lyyyyy23 <130579065+lyyyyy23@users.noreply.github.com>
2023-11-15 16:24:37 +0800

    layout for login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,8 @@
 <template>
+    
     <!-- component -->
-<section class="flex flex-col md:flex-row h-screen items-center">
-
+<section class="flex flex-col md:flex-row h-screen items-center ">
+    
     <div class="bg-indigo-600 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
       <img :src="require('@/assets/images/BG-SOLID-CENTER.jpg')" alt="" class="w-full h-full object-cover">
     </div>
@@ -12,29 +13,33 @@
       <div class="w-full h-100">
   
   
-        <h1 class="text-xl md:text-2xl font-bold leading-tight mt-12">Log in to your account</h1>
+        <h1 class="text-xl md:text-5xl font-bold leading-tight ">Welcome back!</h1>
+        <h1 class="text-xl md:text-2xl font-normal leading-tight mb-12"> We're thrilled to see you again!</h1>
   
         <form class="mt-6" action="#" method="POST">
           <div>
-            <label class="block text-gray-700 text-start">Email Address</label>
-            <input type="email" name="" id="" placeholder="Enter Email Address" class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autofocus autocomplete required>
+            <input type="email" name="" id="" placeholder="Enter Email Address" class="w-full px-5 py-4 rounded-lg bg-gray-100  focus:border-blue-500 focus:bg-white focus:outline-none border-none" autofocus autocomplete required>
           </div>
   
           <div class="mt-4">
-            <label class="block text-gray-700 text-start">Password</label>
-            <input type="password" name="" id="" placeholder="Enter Password" minlength="6" class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500
-                  focus:bg-white focus:outline-none" required>
+            <input type="password" name="" id="" placeholder="Enter Password" minlength="6" class="w-full px-5 py-4 rounded-lg bg-gray-100 mt-1 border focus:border-blue-500 focus:bg-white focus:outline-none border-none" required>
           </div>
   
           <div class="text-right mt-2">
-            <a href="#" class="text-sm font-semibold text-gray-700 hover:text-blue-700 focus:text-blue-700">Forgot Password?</a>
+            <a href="#" class="text-sm font-semibold text-gray-700 hover:text-blue-700 focus:text-blue-700 ">Forgot Password?</a>
           </div>
   
-          <button type="submit" class="w-full block bg-indigo-500 hover:bg-indigo-400 focus:bg-indigo-400 text-white font-semibold rounded-lg
+          <button type="submit" class="w-full block bg-[#EB8B00] hover:bg-[#F9AC3A] focus:bg-[#F9AC3A] text-white font-semibold rounded-lg
                 px-4 py-3 mt-6">Log In</button>
         </form>
   
-        <hr class="my-6 border-gray-300 w-full">
+        <div class="mt-2 mb-7 border-b text-center">
+          <div
+            class="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-3/4"
+          >
+            Or sign up with e-mail
+          </div>
+        </div>
   
         <button type="button" class="w-full block bg-white hover:bg-gray-100 focus:bg-gray-100 text-gray-900 font-semibold rounded-lg px-4 py-3 border border-gray-300">
               <div class="flex items-center justify-center">
@@ -46,9 +51,7 @@
               </div>
             </button>
   
-        <p class="mt-8">Need an account? <a href="#" class="text-blue-500 hover:text-blue-700 font-semibold">Create an
-                account</a></p>
-  
+        
   
       </div>
     </div>
