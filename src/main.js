db4354b6dccadcb7951f53c36ca56187d7ffe285
MDanielle007 <marc.danielle.cabatay219@gmail.com>
2023-11-15 12:54:29 +0800

    Create Project Setup in Tailwind

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import axios from 'axios'
+import './index.css'
+
+axios.defaults.baseURL = 'http://livestockbackend.test/'
 
 createApp(App).use(router).mount('#app')
