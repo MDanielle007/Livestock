5e0084b14f35a5e5237c55a6f5024949f3864159
MDanielle007 <marc.danielle.cabatay219@gmail.com>
2023-11-16 13:26:32 +0800

    Add Farmer Routes

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -6,7 +6,7 @@
             <img :src="require('@/assets/images/BG-SOLID-CENTER.jpg')" alt="" class="w-full h-full object-cover">
         </div>
     
-        <div class="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
+        <div class="bg-white w-full md:max-w-md lg:max-w-full md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
             flex items-center justify-center">
             <div class="w-full h-100">
                 <h1 class="text-xl md:text-5xl font-bold leading-tight ">Welcome back!</h1>
