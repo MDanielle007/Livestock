062953e0a7a2b81c29d103d42591c60c22b04ffa
Marc Danielle Cabatay <127674409+MDanielle007@users.noreply.github.com>
2024-02-11 23:19:42 +0800

    finish setting up farmer notification

--- src/views/farmer/contents/FarmerNotification.vue
+++ src/views/farmer/contents/FarmerNotification.vue
@@ -1,11 +1,15 @@
 <template>
     <div>
-        Farmer Notification
+        <FarmerNotificationDataView/>
     </div>
 </template>
 <script>
+import FarmerNotificationDataView from '@/components/farmer/FarmerNotificationDataView.vue';
+
 export default {
-    
+    components:{
+        FarmerNotificationDataView
+    }
 }
 </script>
 <style>
